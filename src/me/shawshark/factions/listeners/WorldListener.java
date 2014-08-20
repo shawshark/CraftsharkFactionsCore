e7763ee038d8f162ec8bda0d257bbd1e6c8c67d7
Chris <chrissbolding@gmail.com>
2014-08-21 00:10:40 +1000

    Create WorldListener.java

---
+++ src/me/shawshark/factions/listeners/WorldListener.java
@@ -0,0 +1,17 @@
+package me.shawshark.factions.listeners;
+
+import me.shawshark.factions.Factions;
+
+import org.bukkit.event.Listener;
+
+public class WorldListener implements Listener {
+
+	public Factions instance;
+	public Factions getInstance() {
+		return instance;
+	}
+	
+	public WorldListener(Factions instance) {
+		this.instance = instance;
+	}
+}
